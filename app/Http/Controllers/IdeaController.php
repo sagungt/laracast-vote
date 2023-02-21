81eedd710d60f15bd5696633fdb46f51e9e20482
sagungt <agungjordan.aj@gmail.com>
2023-02-21 22:37:47 +0700

    create ideas function

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -18,6 +18,7 @@ class IdeaController extends Controller
         return response()
             ->view('idea.index', [
                 'ideas' => Idea::with('user', 'category', 'status')
+                    ->orderBy('created_at', 'desc')
                     ->simplePaginate(Idea::PAGINATION_COUNT),
             ]);
     }
