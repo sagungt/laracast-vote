13ed79edd42e23ab81bfd941ec5ab9ebcb9960b9
sagungt <agungjordan.aj@gmail.com>
2023-02-21 19:24:55 +0700

    gravatar images & test

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,19 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatarAttribute()
+    {
+        $first_character = $this->email[0];
+        $integer_to_use = is_numeric($first_character)
+            ? ord(strtolower($first_character)) - 21
+            : ord(strtolower($first_character)) - 96;
+
+        return 'https://www.gravatar.com/avatar/'
+            . md5($this->email)
+            . '?s=200'
+            . '&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+            . $integer_to_use
+            . '.png';
+    }
 }
