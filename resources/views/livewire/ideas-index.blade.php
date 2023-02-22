6d3d2eee1f9360157be593f993a564a4c3b77bd7
sagungt <agungjordan.aj@gmail.com>
2023-02-22 14:35:34 +0700

    status filter count

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <div class="filters flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
+    <div class="flex flex-col space-y-3 filters md:flex-row md:space-y-0 md:space-x-6">
         <div class="w-full md:w-1/3">
-            <select name="category" id="category" class="w-full border-none rounded-xl px-4 py-2">
+            <select name="category" id="category" class="w-full px-4 py-2 border-none rounded-xl">
                 <option value="Category One">Category One</option>
                 <option value="Category Two">Category Three</option>
                 <option value="Category Four">Category Four</option>
@@ -9,15 +9,15 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select name="filter" id="filter" class="w-full border-none rounded-xl px-4 py-2">
+            <select name="filter" id="filter" class="w-full px-4 py-2 border-none rounded-xl">
                 <option value="filter One">filter One</option>
                 <option value="filter Two">filter Three</option>
                 <option value="filter Four">filter Four</option>
                 <option value="filter Five">filter Five</option>
             </select>
         </div>
-        <div class="w-full md:w-2/3 relative">
-            <input type="search" name="search" id="search" placeholder="Find an idea" class="w-full rounded-xl bg-white px-4 py-2 pl-8 border-none placeholder:text-gray-900">
+        <div class="relative w-full md:w-2/3">
+            <input type="search" name="search" id="search" placeholder="Find an idea" class="w-full px-4 py-2 pl-8 bg-white border-none rounded-xl placeholder:text-gray-900">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-700" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -26,7 +26,7 @@
         </div>
     </div> <!-- end filters -->
     
-    <div class="ideas-container space-y-6 my-6">
+    <div class="my-6 space-y-6 ideas-container">
         @foreach ($ideas as $idea)
             <livewire:idea-index
                 :key="$idea->id"
@@ -37,6 +37,7 @@
     </div> <!-- end ideas-container -->
     
     <div class="my-8">
-        {{ $ideas->links() }}
+        {{-- {{ $ideas->links() }} --}}
+        {{ $ideas->appends(request()->query())->links() }}
     </div>
 </div>
