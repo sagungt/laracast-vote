3329939f57fe08c76636c496bf30763960c69cdf
sagungt <agungjordan.aj@gmail.com>
2023-02-25 20:27:45 +0700

    badge count and skeleton loader

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -6,7 +6,10 @@ use Livewire\Component;
 
 class CommentNotifications extends Component
 {
+    const NOTIFIATION_TRESHOLD = 20;
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
 
     protected $listeners = [
         'getNotifications',
@@ -15,11 +18,29 @@ class CommentNotifications extends Component
     public function mount()
     {
         $this->notifications = collect([]);
+        $this->isLoading = true;
+        $this->getNotificationCount();
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if ($this->notificationCount > self::NOTIFIATION_TRESHOLD) {
+            $this->notificationCount = self::NOTIFIATION_TRESHOLD . '+';
+        }
     }
 
     public function getNotifications()
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()
+            ->user()
+            ->unreadNotifications()
+            ->latest()
+            ->take(self::NOTIFIATION_TRESHOLD)
+            ->get();
+
+        $this->isLoading = false;
     }
 
     public function render()
