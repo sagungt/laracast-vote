07bb3e8001366e4c599995dc6d0771ec8f8ef500
sagungt <agungjordan.aj@gmail.com>
2023-02-25 19:13:29 +0700

    notificatoin styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,7 @@
                 @if (Route::has('login'))
                     <div class="p-6">
                         @auth
+                        <div class="flex items-center space-x-4">
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
@@ -37,6 +38,55 @@
                                     {{ __('Log Out') }}
                                 </a>
                             </form>
+
+                            <div
+                                class="relative"
+                                x-data="{ isOpen: false }"
+                            >
+                                <button x-on:click="isOpen = !isOpen">
+                                    <svg viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 text-gray-400">
+                                        <path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd" />
+                                    </svg>
+                                    <div class="absolute rounded-full bg-red text-white text-xxs w-6 h-6 flex justify-center items-center -top-1.5 -right-1.5 border-2">1</div>
+                                </button>
+                                <ul
+                                    x-cloak
+                                    x-transition.origin.top
+                                    x-show="isOpen"
+                                    x-on:click.away="isOpen = false"
+                                    x-on:keydown.escape.window="isOpen = false"
+                                    class="absolute z-10 ml-8 text-left bg-white top-8 w-76 md:w-96 shadow-dialog rounded-xl -right-32 md:-right-12 max-h-128 overflow-y-auto text-gray-700"
+                                >
+                                    @foreach (range(1, 5) as $item)
+                                        <li>
+                                            <a
+                                                href="#"
+                                                class="flex px-5 py-3 transition duration-150 text-xs ease-in hover:bg-gray-100"
+                                            >
+                                                <img src="https://www.gravatar.com/avatar/0000000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                                                <div class="ml-4">
+                                                    <div>
+                                                        <span class="font-semibold">
+                                                            jondo
+                                                        </span> commented on 
+                                                        <span class="font-semibold">
+                                                            My idae
+                                                        </span>:
+                                                        <span class="line-clamp-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto voluptates, officia ut dolorum vel sapiente magnam eum saepe, fugit quae inventore reiciendis ab rem temporibus! Ipsa recusandae sed at iste, ab alias et amet voluptatibus laudantium magnam aspernatur veniam illum."</span>
+                                                    </div>
+                                                    <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                                </div>
+                                            </a>
+                                        </li>
+                                    @endforeach
+                                    <li class="border-t border-gray-300 text-center">
+                                        <button class="block font-semibold px-5 py-4 transition duration-150 text-xs ease-in hover:bg-gray-100 w-full">
+                                            Mark all as read
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                         @else
                             <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
